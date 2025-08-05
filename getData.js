71e365ec5e8d7a8de89422dbb8569b280be4efc5
pabloosss <notingss@gmail.com>
2025-08-05 12:45:16 +0200

    Update getData.js

--- getData.js
+++ getData.js
@@ -8,5 +8,8 @@ exports.handler = async () => {
   const octo = new Octokit({ auth: TOKEN });
   const { data } = await octo.repos.getContent({ owner: OWNER, repo: REPO, path: PATH });
   const content = Buffer.from(data.content, "base64").toString();
-  return { statusCode: 200, body: content };
+  return {
+    statusCode: 200,
+    body: content
+  };
 };
